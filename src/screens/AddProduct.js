f756ad3eb4d8bd0c1c3047a4a2d839987c56cdef
Aditya Rajput <adityarajput751@gmail.com>
2023-07-25 15:48:11 +0530

    ok

--- src/screens/AddProduct.js
+++ src/screens/AddProduct.js
@@ -176,7 +176,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   selectedCategory: {
-    backgroundColor: 'lightblue',
+    backgroundColor: 'blue',
   },
   button: {
     backgroundColor: 'blue',
